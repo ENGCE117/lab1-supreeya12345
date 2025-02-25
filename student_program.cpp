f25fbeff599c75e498e5192c1ba650ac19e2b83e
SUPREEYA <ormsupreeya@gmail.com>
2025-02-25 13:45:03 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void GetMatrix(int **value, int *row, int *col);
+void PrintMatrix(int **value, int row, int col);
 
 int main() {
-    //YOUR CODE HERE
+    int *data, m = 3, n = 3 ;
+    data = new int[ m * n ] ;
+    GetMatrix( &data, &m, &n ) ;
+    PrintMatrix( &data, m, n ) ;
     return 0 ;
 }//end function
+
+void GetMatrix(int **value, int *row, int *col) {
+    printf("Enter matrix values:\n");
+    for (int i = 0; i < (*row) * (*col); i++) {
+        scanf("%d", (*value + i));
+    }
+}
+
+void PrintMatrix(int **value, int row, int col) {
+    printf("Matrix (%dx%d):\n", row, col);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            printf("%d ", (*value)[i * col + j]);
+        }
+        printf("\n");
+    }
+}
+
